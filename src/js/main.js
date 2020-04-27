7995aa413352aeca7eb6b84467beae3ff77e208a
Pawel Mrozikowski <mrozik.p@gmail.com>
2020-04-27 02:44:53 +0200

    project section: correct js

--- src/js/main.js
+++ src/js/main.js
@@ -13,13 +13,19 @@ fetch('https://api.github.com/users/PawelMrozikowski/repos')
         console.log(repo);
     list.innerHTML +=`
     <div class="card">
+            
             <img class="card__icon" src="assets/img/card_icon.svg" alt="github_icon.">
 
             <h4 class="card__header">${name} </h4>
-            <p class="card__paragraph">${description}</p>
+            ${
+              description ? `<p class="card__paragraph">${description}</p>` : `<p class="card__paragraph"> No description</p>`
+            }
+          
 
             <div class="card__footer">
-              <a class="card__footer-link" href="${homepage}" target="_blank" rel="nofollow noreferrer" title="Demo: ${name}">Demo ></a>
+            ${
+              homepage ? `<a class="card__footer-link" href="${homepage}" target="_blank" rel="nofollow noreferrer" title="Demo: ${name}">Demo ></a>` : ''
+            }
               <a class="card__footer-link card__footer-link--source" href="${html_url}" target="_blank" rel="nofollow noreferrer" title="Source code: ${name}">GitHub ></a>
             </div>
           </div>`
